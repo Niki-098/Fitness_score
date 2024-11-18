5ac591e5ef170b5a9ad43bdbbc7bc14a02a212b1
Nikita <kniki098@gmail.com>
2024-11-19 00:28:23 +0530

    Created healthscore calculation page

--- fitness_health/users/urls.py
+++ fitness_health/users/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('trends/', views.trends, name='trends'), 
     path('login/',views.login,name='login'),
+    path('landing_page/',views.landing_page,name='landing_page'),
+    path('calculate_score/',views.calculate_score,name='calculate_score'),
+    
 ]
--- fitness_health/users/views.py
+++ fitness_health/users/views.py
@@ -10,3 +10,9 @@ def trends(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+def calculate_score(request):
+    return render(request, 'score.html') 
